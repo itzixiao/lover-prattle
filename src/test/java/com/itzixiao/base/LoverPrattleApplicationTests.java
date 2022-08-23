318f95e32c719e1ee561bab32c79a8b61b449e64
changsc <changsc@ztjzsoft.com>
2022-08-23 10:46:10 +0800

    fix(): 修改图片地址为base64编码,防止图片链接失效

--- src/test/java/com/itzixiao/base/LoverPrattleApplicationTests.java
+++ src/test/java/com/itzixiao/base/LoverPrattleApplicationTests.java
@@ -1,13 +1,19 @@
 package com.itzixiao.base;
 
+import com.itzixiao.scheduled.MyScheduled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LoverPrattleApplicationTests {
 
+    @Autowired
+    MyScheduled scheduled;
+
     @Test
-    void contextLoads() {
+    public void dsrw() {
+        scheduled.dsrw();
     }
 
 }
