8d25c0e927f7e55c8c7f086139b511b98f88d579
changsc <itzixiao@foxmail.com>
2024-12-08 12:33:23 +0800

    fix(): 使用 SSL/TLS 加密第三方邮件客户端

--- src/main/java/com/itzixiao/base/SendMessage.java
+++ src/main/java/com/itzixiao/base/SendMessage.java
@@ -1,6 +1,5 @@
 package com.itzixiao.base;
 
-
 import com.alibaba.fastjson.JSON;
 import com.itzixiao.utils.HttpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +17,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
-
 @Component
 public class SendMessage {
 
     @Autowired
     private JavaMailSenderImpl mailSender;
+
+    @Value("${spring.mail.api}")
+    private String api;
 
     @Value("${spring.mail.username}")
     private String from;
@@ -63,7 +64,7 @@ public class SendMessage {
         String process = templateEngine.process("email", context);
 
         MimeMessage mineMessage = mailSender.createMimeMessage();
-        //要带附件第二个参数设为true
+        // 要带附件第二个参数设为true
         MimeMessageHelper helper = new MimeMessageHelper(mineMessage, true);
         helper.setFrom(from);
         helper.setTo(sheMail);
@@ -80,12 +81,12 @@ public class SendMessage {
 
     /**
      * 调用文件
+     *
      * @return
      */
     public String getOneS() {
-        String result = HttpUtils.sendGet("https://api.shadiao.app/chp",null);
+        String result = HttpUtils.sendGet(api, null);
         Map<?, ?> map = JSON.parseObject(result, Map.class);
-        String text = (String) ((Map<?, ?>) map.get("data")).get("text");
-        return text;
+        return (String) ((Map<?, ?>) map.get("data")).get("text");
     }
 }
